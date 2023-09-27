4ea9180c7bf34b9a6128b43c43804e786aaadb93
Nrezq12 <d.5831288@gmail.com>
2023-09-28 00:29:58 +0300

    f1

--- app/utils/verifyToken.js
+++ app/utils/verifyToken.js
@@ -3,15 +3,15 @@ import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) {
-    return next(createError(401, "Youxxx are not authenticated!"));
-  }
+  // if (!token) {
+  //   return next(createError(401, "Youxxx are not authenticated!"));
+  // }
 
-  // jwt.verify(token, process.env.JWT, (err, user) => {
-  //   if (err) return next(createError(403, "Token is not valid!"));
-  //   req.user = user;
-  //   next();
-  // });
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+    next();
+  });
 };
 
 export const verifyUser = (req, res, next) => {
