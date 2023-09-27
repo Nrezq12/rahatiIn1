e8d1909516f9b275868e22ed98011c8694e50bf9
Nrezq12 <d.5831288@gmail.com>
2023-09-28 00:42:20 +0300

    f1

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,7 +5,6 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js";
-import { verifyUser,verifyAdmin} from "../utils/verifyToken.js";
 
 const router = express.Router();
 
@@ -13,26 +12,26 @@ router.get("/checkauthentication", (req,res,next)=>{
   res.send("hello user, you are logged in")
 })
 
-router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+router.get("/checkuser/:id", (req,res,next)=>{
   res.send("hello user, you are logged in and you can delete your account")
 })
 
-router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+router.get("/checkadmin/:id", (req,res,next)=>{
   res.send("hello admin, you are logged in and you can delete all accounts")
 })
 
 
 //UPDATE
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 
 //DELETE
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 
 //GET
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 
 //GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", getUsers);
 
 
 export default router;
