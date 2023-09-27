6cbaaca4cf3b98bec4efe2576fd38160b8f4a484
Nrezq12 <d.5831288@gmail.com>
2023-09-27 14:52:20 +0300

    f

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -31,7 +31,7 @@ const New = ({ inputs, title }) => {
         img: url,
       };
 
-      await axios.post("/auth/register", newUser);
+      await axios.post("https://rahati-in7.onrender.com/api/auth/register", newUser);
     } catch (err) {
       console.log(err);
     }
